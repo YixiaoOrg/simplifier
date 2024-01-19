c05c2fbd240a6e7d8baa1c315501b10836eda5a0
Yixiao <86191112+YixiaoOrg@users.noreply.github.com>
2024-01-19 17:34:46 +0800

    实时获取评论者头像

--- comments.php
+++ comments.php
@@ -1,31 +1,28 @@
 <?php
+
+
+
+
+error_reporting(0);
 function threadedComments($comments, $options) {
     $cby = $comments->authorId == $comments->ownerId ? '<span class="cby">admin</span>' : '';
     $clevel = $comments->levels > 0 ? 'c_c' : 'c_p';
-    $author = $comments->url ? '<a href="' . $comments->url . '"'.'" target="_blank"' . ' rel="external">' . $comments->author . '</a>' : $comments->author;
+    $author = $comments->url ? '<a href="' . $comments->url . '" target="_blank" rel="external">' . $comments->author . '</a>' : $comments->author;
 ?>
 <li id="li-<?php $comments->theId(); ?>" class="<?php echo $clevel;?>">
-<div id="<?php $comments->theId(); ?>">
-<?php
-    $t = 1209600*8;
-    $a = Helper::options()->siteUrl . 'usr/uploads/avatar/' . md5(strtolower($comments->mail)) . '.jpg';
-    $e = __TYPECHO_ROOT_DIR__ . '/' . 'usr/uploads/avatar/' . md5(strtolower($comments->mail)) . '.jpg';
-    if ( !is_file($e) || (time() - filemtime($e)) > $t ){
-    $a = 'https://cdn.v2ex.com/gravatar/' . md5(strtolower($comments->mail)) . '?s=80&r=X&d=';
-    copy($a, $e);
-        if (!is_file($e)){
-            copy($d, $e);
-        };
-    };
-    if ( filesize($e) < 900 ) copy($d, $e);
-?>
-    <img class="avatar" src="<?php echo $a ?>" alt="<?php echo $comments->author; ?>" />
-    <div class="cp">
-    <?php $comments->content(); ?>
-        <div class="cm"><span class="ca"><?php echo $author ?></span>&nbsp;<?php echo $cby;?>&nbsp;<?php $comments->date(); ?><span class="cr"><?php $comments->reply(); ?></span></div>
+    <div id="<?php $comments->theId(); ?>">
+        <?php
+        $a = 'https://cravatar.cn/avatar/' . md5(strtolower($comments->mail)) . '?s=80&r=X&d=';
+        ?>
+        <img class="avatar" src="<?php echo $a ?>" alt="<?php echo $comments->author; ?>" />
+        <div class="cp">
+            <?php $comments->content(); ?>
+            <div class="cm">
+                <span class="ca"><?php echo $author ?></span>&nbsp;<?php echo $cby;?>&nbsp;<?php $comments->date(); ?><span class="cr"><?php $comments->reply(); ?></span>
+            </div>
+        </div>
     </div>
-</div>
-<?php if ($comments->children){ ?><div class="children"><?php $comments->threadedComments($options); ?></div><?php } ?>
+    <?php if ($comments->children){ ?><div class="children"><?php $comments->threadedComments($options); ?></div><?php } ?>
 </li>
 <?php } ?>
 <style type="text/css">
@@ -66,7 +63,7 @@ textarea.ci{padding-top:8px;height:10rem;resize:none}
     }
   }
   </script>
-
+  
 <div id="comments" class="cf">
 <?php $this->comments()->to($comments); ?>
 <?php if ($comments->have()): ?>
@@ -81,20 +78,21 @@ textarea.ci{padding-top:8px;height:10rem;resize:none}
     <span>已登入<a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->logoutUrl(); ?>" title="Logout">退出 &raquo;</a></span>
 <?php else: ?>
     <?php if($this->remember('author',true) != "" && $this->remember('mail',true) != "") : ?>
-        <span>欢迎【<?php $this->remember('author'); ?>】的归来 | <small style="cursor: pointer;" onclick = "tg_c('ainfo','hinfo');"> 编辑资料</small></span>
-          <div id ="ainfo" class="ainfo hinfo"style="display: none;">
+        <span>欢迎【<?php $this->remember('author'); ?>】的归来 | <small style="cursor: pointer;" onclick = "tg_c('ainfo','hinfo');"> 编辑资料</small>
+</span>
+        <div id ="ainfo" class="ainfo hinfo"style="display: none;">
     <?php else : ?>
         <div class="ainfo">
         <?php endif ; ?>
         <div class="tbox">
-        <input type="text" name="author" id="author" class="ci" placeholder="称呼" value="<?php $this->remember('author'); ?>" required>
-        <input type="email" name="mail" id="mail" class="ci" placeholder="邮箱" value="<?php $this->remember('mail'); ?>" required>
+        <input type="text" name="author" id="author" class="ci" placeholder="称呼*" value="<?php $this->remember('author'); ?>" required>
+        <input type="email" name="mail" id="mail" class="ci" placeholder="邮箱*" value="<?php $this->remember('mail'); ?>" required>
         <input type="url" name="url" id="url" class="ci" placeholder="http://" value="<?php $this->remember('url'); ?>">
         </div>
         </div>
     <?php endif; ?>
     <div class="tbox"><textarea name="text" id="textarea" class="ci" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};" placeholder="在这里输入你的评论" required ><?php $this->remember('text',false); ?></textarea></div>
-    <button type="submit" class="submit" id="submit">提交评论 (Ctrl + Enter)</button>
+<button type="submit" class="submit" id="submit">提交评论 (Ctrl + Enter)</button>
 </form>
 </div>
 </div>
